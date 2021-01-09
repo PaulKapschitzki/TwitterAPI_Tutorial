4224679683a645250873b008ea31ae59d653d37d
PaulKapschitzki <kapschitzki.p@web.de>
2021-01-09 16:41:42 +0100

    Changed rules and timeout till removal from html

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'trading' }, {value: 'long'}];
+const rules = [{ value: 'trading' }];
 
 // Get stream rules
 const getRules = async () => {
