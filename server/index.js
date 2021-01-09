58248b6f0c87d7199feb295a7079cc0312b6fc76
PaulKapschitzki <kapschitzki.p@web.de>
2021-01-09 16:35:14 +0100

    Finalised tutorial for streaming tweets Hardcoded rules determine what kind of tweets are postet to index.html

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketIo = require('socket.io');
 const needle = require('needle'); // http request client
+const { RSA_NO_PADDING } = require('constants');
 const config = require('dotenv').config();
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'));
+});
 
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'giveaway' }];
+const rules = [{ value: 'coding' }];
 
 // Get stream rules
 const getRules = async () => {
@@ -19,28 +34,12 @@ const getRules = async () => {
     return response.body;
 };
 
-const streamTweets = () => {
-    const stream = needle.get(streamURL, {
-        headers: {
-            Authorization: `Bearer ${TOKEN}`
-        }
-    });
-
-    stream.on('data', (data) => {
-        try {
-            const json = JSON.parse(data);
-            console.log(json);
-        } catch (error) {
-            // leaving catch block empty will leave the connection open even if there are no tweets
-        }
-    })
-}
-
 // Set stream rules
 const setRules = async () => {
     const data = {
         add: rules
     }
+    
     const response = await needle('post', rulesURL, data, {
         headers: {
             'content-type': 'application/json',
@@ -52,7 +51,8 @@ const setRules = async () => {
 };
 
 // Delete stream rules
-const deleteRules = async () => {
+const deleteRules = async (rules) => {
+    console.log("deleteRules function call")
     if (!Array.isArray(rules.data)) {
         return null;
     }
@@ -65,7 +65,7 @@ const deleteRules = async () => {
         }
     }
 
-    const response = await needle('put', rulesURL, data, {
+    const response = await needle('post', rulesURL, data, {
         headers: {
             'content-type': 'application/json',
             Authorization: `Bearer ${TOKEN}`
@@ -75,7 +75,29 @@ const deleteRules = async () => {
     return response.body;
 };
 
-(async () => {
+
+// Emit tweet stream
+const streamTweets = (socket) => {
+    const stream = needle.get(streamURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`
+        }
+    });
+
+    stream.on('data', (data) => {
+        try {
+            const json = JSON.parse(data);
+            // console.log(json);
+            socket.emit('tweet', json);
+        } catch (error) {
+            // leaving catch block empty will leave the connection open even if there are no tweets
+        }
+    })
+}
+
+io.on('connection', async () => {
+    console.log('Client connected...');
+
     let currentRules;
     
     try {
@@ -83,7 +105,7 @@ const deleteRules = async () => {
         currentRules = await getRules();
 
         // Delete all stream rules
-        await deleteRules(currentRules);
+        deleteRules(currentRules);
 
         // Set rules based on array above
         await setRules();
@@ -92,5 +114,41 @@ const deleteRules = async () => {
         process.exit(1);
     }
 
-    streamTweets();
-})();
+    streamTweets(io);
+});
+
+// (async () => {
+//     let currentRules;
+    
+//     try {
+//         // Get all stream rules
+//         currentRules = await getRules();
+
+//         // Delete all stream rules
+//         await deleteRules(currentRules);
+
+//         // Set rules based on array above
+//         await setRules();
+//     } catch (error) {
+//         console.error(error);
+//         process.exit(1);
+//     }
+
+//     streamTweets();
+// })();
+
+// // Reconnecting on error
+// const filteredStream = streamTweets(io)
+
+// let timeout = 0
+// filteredStream.on('timeout', () => {
+//   // Reconnect on error
+//   console.warn('A connection error occurred. Reconnecting…')
+//   setTimeout(() => {
+//     timeout++
+//     streamTweets(io)
+//   }, 2 ** timeout)
+//   streamTweets(io)
+// });
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
