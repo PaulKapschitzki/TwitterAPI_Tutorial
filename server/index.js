1bad673158158b307af2a9d46ebadbf98ebb3a10
PaulKapschitzki <kapschitzki.p@web.de>
2021-01-30 15:04:12 +0100

    Changed search item

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'trading' }];
+const rules = [{ value: 'Bitcoin' }];
 
 // Get stream rules
 const getRules = async () => {
